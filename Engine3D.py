1e405c576693ae69f617a26178edcfc8f34f9856
danicabrera <69865919+danicabrera@users.noreply.github.com>
2022-08-29 23:35:40 -0600

    Cambios

--- Engine3D.py
+++ Engine3D.py
@@ -12,16 +12,18 @@ from texture import Texture
 width = 960
 height = 540
 rend = Renderer(width, height)
-rend.active_shader = glow
+rend.active_shader = gourad
 rend.active_texture = Texture("model.bmp")
-rend.glLoadModel("model.obj",
+rend.glLoadModel("Penguin.obj",
                  translate= V3(3, 0, -10),
                  scale = V3(4, 4, 4))
 
 
-rend.active_shader = Intense
+rend.active_shader = Ice
+rend.active_texture = Texture("Model.bmp")
 rend.glLoadModel("model.obj",
                  translate= V3(-3, 0, -10),
                  scale = V3(4, 4, 4))
+
 
 rend.glFinish("output.bmp")
--- shader.py
+++ shader.py
@@ -4,6 +4,7 @@
 #Base de Carlos Alonso
 
 import numpy as np
+import random as rand
 
 def flat(render, **kwargs):
 
@@ -321,6 +322,92 @@ def Ice(render, **kwargs):
     else:
         return 0,0,0
 
+def Comic(render, **kwargs):
+
+    u, v, w = kwargs["baryCoords"]
+    b, g, r = kwargs["vColor"]
+    tA, tB, tC = kwargs["texCoords"]
+    nA, nB, nC = kwargs["normals"]
+    indice = 0
+    b /= 255
+    g /= 255
+    r /= 255
+
+    if render.active_texture:
+        # P = Au + Bv + Cw
+        tU = tA[0] * u + tB[0] * v + tC[0] * w
+        tV = tA[1] * u + tB[1] * v + tC[1] * w
+
+        texColor = render.active_texture.getColor(tU, tV)
+
+        b *= texColor[2]
+        g *= texColor[1]
+        r *= texColor[0]
+
+    triangleNormal = np.array([nA[0] * u + nB[0] * v + nC[0] * w,
+                               nA[1] * u + nB[1] * v + nC[1] * w,
+                               nA[2] * u + nB[2] * v + nC[2] * w])
+
+    dirLight = np.array(render.dirLight)
+    intensity = np.dot(triangleNormal, -dirLight)
+
+    if intensity < 0.2:
+        intensity = 0.1
+        indice = 9
+    elif intensity < 0.3:
+        intensity = 0.2
+        indice = 8
+    elif intensity < 0.4:
+        intensity = 0.3
+        indice = 7
+    elif intensity < 0.5:
+        intensity = 0.4
+        indice = 6
+    elif intensity < 0.6:
+        intensity = 0.5
+        indice = 5
+    elif intensity < 0.7:
+        intensity = 0.6
+        indice = 4
+    elif intensity < 0.8:
+        intensity = 0.7
+        indice = 3
+    elif intensity < 0.9:
+        intensity = 0.8
+        indice = 2
+    elif intensity <= 1:
+        intensity = 0.9
+        indice = 1
+
+    b *= intensity
+    g *= intensity
+    r *= intensity
+
+    camForward = (render.camMatrix.item(0,2),
+                  render.camMatrix.item(1,2),
+                  render.camMatrix.item(2,2))
+
+    glowAmount = 1 - np.dot(triangleNormal, camForward)
+
+    if glowAmount <= 0: glowAmount = 0
+
+    valor = rand.randint(0,indice)
+
+    color1 = (0.2,0.8,1)
+
+    b += color1[2] * 0.5
+    g += color1[1] * 0.5
+    r += color1[0] * 0.5
+
+    if b > 1: b = 1
+    if g > 1: g = 1
+    if r > 1: r = 1
+
+    if intensity > 0:
+        return r, g, b
+    else:
+        return 0,0,0
+
 def Intense(render, **kwargs):
 
     u, v, w = kwargs["baryCoords"]
@@ -357,15 +444,7 @@ def Intense(render, **kwargs):
     elif intensity < 0.4:
         intensity = 0.3
     elif intensity < 0.5:
-        intensity = 0.4
-    elif intensity < 0.6:
-        intensity = 0.5
-    elif intensity < 0.7:
-        intensity = 0.6
-    elif intensity < 0.8:
-        intensity = 0.7
-    elif intensity < 0.9:
-        intensity = 0.8
+        intensity = 0.9
     elif intensity <= 1:
         intensity = 0.9
 
@@ -377,10 +456,22 @@ def Intense(render, **kwargs):
                   render.camMatrix.item(1,2),
                   render.camMatrix.item(2,2))
 
-    glowAmount = 1 - np.dot(triangleNormal, camForward)
+    glowAmount = 0 + np.dot(triangleNormal, camForward)
 
     if glowAmount <= 0: glowAmount =0
+    color1 = (0.2, 0.8, 1)
 
+
+    if glowAmount <= 0.3:
+
+        b += color1[2] * 0.3
+        g += color1[1] * 0.3
+        r += color1[0] * 0.3
+    elif glowAmount <= 0.9:
+
+        b += color1[2] * 0.5
+        g += color1[1] * 0.5
+        r += color1[0] * 0.5
 
 
     if b > 1: b = 1
